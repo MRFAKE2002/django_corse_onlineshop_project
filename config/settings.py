2e0500936e240a4e29f9709b7056c92265340826
ROOZBEH <MR.FAKE@gmail.com>
2022-09-09 11:28:28 +0430

    add login and signup and logout

--- config/settings.py
+++ config/settings.py
@@ -41,12 +41,24 @@ INSTALLED_APPS = [
     
     # Third party
     'crispy_forms',
+    'allauth',
+    'allauth.account',
     
     # My apps
     'accounts',
     'pages',
 
 ]
+
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -145,6 +157,25 @@ LOGIN_REDIRECT_URL = 'home'
 
 LOGOUT_REDIRECT_URL = 'home'
 
+# signup get email
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 # crispy forms for config
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+# allauth config  
+
+ACCOUNT_SESSION_REMEMBER = True  
+
+ACCOUNT_SIGNUP_PASSWORD_ENTER_TWICE = False
+
+ACCOUNT_AUTHENTICATION_METHOD = 'email'
+
+ACCOUNT_USERNAME_REQUIRED = False
+
+ACCOUNT_EMAIL_REQUIRED = True
+
+ACCOUNT_UNIQUE_EMAIL = True
+
