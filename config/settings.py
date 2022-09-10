4d6354bf22d68835ec90ff1e5a95f1560ffe7533
ROOZBEH <MR.FAKE@gmail.com>
2022-09-11 00:13:26 +0430

    make products app and make its model

--- config/settings.py
+++ config/settings.py
@@ -49,8 +49,9 @@ INSTALLED_APPS = [
     'allauth.account',
     
     # My apps
-    'accounts',
-    'pages',
+    'accounts.apps.AccountsConfig',
+    'pages.apps.PagesConfig',
+    'products.apps.ProductsConfig',
 
 ]
 
