0d70501a6ef758f49a36b993e34aa740e46ae407
ROOZBEH <MR.FAKE@gmail.com>
2022-09-11 18:53:53 +0430

    add product detail view and template

--- products/views.py
+++ products/views.py
@@ -10,3 +10,9 @@ class ProductListView(generic.ListView):
     context_object_name = 'products'    
 
 
+class ProductDetailView(generic.DetailView):
+    model = Product
+    template_name = 'products/product_detail.html'
+    context_object_name = 'product'
+
+
