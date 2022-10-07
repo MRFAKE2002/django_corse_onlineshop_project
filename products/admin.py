258fda21e18e1ae244a12216955907a81292fe57
ROOZBEH <MR.FAKE@gmail.com>
2022-10-07 13:28:38 +0330

    add templatetags and comment inlines in project

--- products/admin.py
+++ products/admin.py
@@ -2,12 +2,24 @@ from django.contrib import admin
 
 from .models import Product, Comment
 
+
+# We can use TabularInline or StackedInline in admin to display comments in product line 
+class ProductCommentInline(admin.StackedInline):
+    model = Comment
+    fields = ['author', 'body', 'stars', 'active',]
+
+
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'price', 'active',]
+    
+    inlines = [
+        ProductCommentInline,
+    ]
 
 
-@admin.register(Comment)
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ['product', 'author', 'body', 'stars', 'active',]
+# @admin.register(Comment)
+# class CommentAdmin(admin.ModelAdmin):
+#     list_display = ['product', 'author', 'body', 'stars', 'active',]
 
--- products/models.py
+++ products/models.py
@@ -17,7 +17,17 @@ class Product(models.Model):
     def get_absolute_url(self):
         return reverse('product_detail', args=[self.pk])
     
-    
+
+# We can make a custom manager here for set our queryset for models 
+# In " Comment.objects " objects is manager and its work is get something from Comment model
+# This custom manager give us all comments that they are active
+# default manager " comments.objects.filter(active=True) "
+class ActiveCommentManager(models.Manager):
+    def get_queryset(self):
+        return super(ActiveCommentManager, self).get_queryset().filter(active=True)
+
+
+
 class Comment(models.Model):
     PRODUCT_STAR = [
         ('1', 'Very Bad'),
@@ -29,14 +39,20 @@ class Comment(models.Model):
     
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments')
     author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='comments')
-    body = models.TextField()
-    stars = models.CharField(max_length=10, choices=PRODUCT_STAR, blank=True)
+    body = models.TextField(verbose_name='Comment Text')
+    stars = models.CharField(max_length=10, choices=PRODUCT_STAR, blank=True, verbose_name='What is your score?')
     
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
         
     active = models.BooleanField(default=True)
     
-    def get_absolute_url(self):
-        return reverse()
+    # Manager
+    # This is default code
+    objects = models.Manager()
+    # Now we can use this code " comments.active_comment_manager " instead of " comments.objects.filter(active=True) "
+    active_comment_manager = ActiveCommentManager()
     
+    def get_absolute_url(self):
+        return reverse('product_detail', args=[self.product.id])
+
