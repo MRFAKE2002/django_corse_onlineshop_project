9bc705461bf0e55be37569a04bec4c757166ce45
ROOZBEH <MR.FAKE@gmail.com>
2022-10-13 22:58:46 +0330

    use gettext_lazy to translate our verbose_name

--- products/models.py
+++ products/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.shortcuts import reverse
 from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _ # we use this function to translate our verbose_name
 
 class Product(models.Model):
     title = models.CharField(max_length=100)
@@ -39,8 +40,8 @@ class Comment(models.Model):
     
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments')
     author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='comments')
-    body = models.TextField(verbose_name='Comment Text')
-    stars = models.CharField(max_length=10, choices=PRODUCT_STAR, blank=True, verbose_name='What is your score?')
+    body = models.TextField(verbose_name=_('Comment Text'))
+    stars = models.CharField(max_length=10, choices=PRODUCT_STAR, blank=True, verbose_name=_('What is your score?'))
     
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
