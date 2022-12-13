8b931deb1fa0c8ef8506e85a00f4e6b3ebcc1219
ROOZBEH <MR.FAKE@gmail.com>
2022-12-13 18:27:44 +0330

    add Order and OrderItem in admin

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from jalali_date.admin import ModelAdminJalaliMixin
 
-# Register your models here.
+from .models import Order, OrderItem
+
+
+# We can use TabularInline or StackedInline in admin to display comments in product line 
+class OrderItemInline(admin.StackedInline):
+    model = OrderItem
+    fields = ['order', 'product', 'quantity', 'price']
+    extra = 1
+
+@admin.register(Order)
+class OrderAdmin(ModelAdminJalaliMixin, admin.ModelAdmin):
+    list_display = ['user', 'first_name', 'last_name', 'datetime_created', 'is_paid',]
+    
+    inlines = [
+        OrderItemInline,
+    ]
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ['order', 'product', 'quantity', 'price']
+
