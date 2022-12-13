5214b07713b0423e31b644a2df43c75235a0183d
ROOZBEH <MR.FAKE@gmail.com>
2022-12-13 18:31:44 +0330

    change something

--- orders/admin.py
+++ orders/admin.py
@@ -5,7 +5,7 @@ from .models import Order, OrderItem
 
 
 # We can use TabularInline or StackedInline in admin to display comments in product line 
-class OrderItemInline(admin.StackedInline):
+class OrderItemInline(admin.TabularInline):
     model = OrderItem
     fields = ['order', 'product', 'quantity', 'price']
     extra = 1
